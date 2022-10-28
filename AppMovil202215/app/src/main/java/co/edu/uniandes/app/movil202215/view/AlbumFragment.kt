72820439a08e875e4fdad35858de714c34eb851f
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-10-28 11:21:32 -0500

    Corrección de ubicación de clases

--- AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/view/AlbumFragment.kt
+++ AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/view/AlbumFragment.kt
@@ -1,10 +1,11 @@
-package co.edu.uniandes.app.movil202215
+package co.edu.uniandes.app.movil202215.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import co.edu.uniandes.app.movil202215.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Colecciones.newInstance] factory method to
+ * Use the [Vinilos.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Colecciones : Fragment() {
+class AlbumFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class Colecciones : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_colecciones, container, false)
+        return inflater.inflate(R.layout.fragment_vinilos, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class Colecciones : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Colecciones.
+         * @return A new instance of fragment Vinilos.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Colecciones().apply {
+            AlbumFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
