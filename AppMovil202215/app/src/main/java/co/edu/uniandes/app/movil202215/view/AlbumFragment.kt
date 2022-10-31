db4e29e42e6a21f761f90078bc77a76c80062126
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-10-31 07:09:23 -0500

    Se agrega integración de vista con consulta de datos en la opción de … (#46)
    * Se agrega integración de vista con consulta de datos en la opción de álbumes #19
    
    * Se sube apk provisional
    
    * Se actualiza README

--- AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/view/AlbumFragment.kt
+++ AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/view/AlbumFragment.kt
@@ -1,54 +1,72 @@
 package co.edu.uniandes.app.movil202215.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.edu.uniandes.app.movil202215.R
+import co.edu.uniandes.app.movil202215.databinding.AlbumFragmentBinding
+import co.edu.uniandes.app.movil202215.models.Album
+import co.edu.uniandes.app.movil202215.view.adapters.AlbumsAdapter
+import co.edu.uniandes.app.movil202215.viewmodels.AlbumViewModel
 
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
 class AlbumFragment : Fragment() {
-
-    private lateinit var adapter : AlbumAdapter
+    private var _binding: AlbumFragmentBinding? = null
+    private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
-    private lateinit var albumList: ArrayList<Album>
-
-    private lateinit var image: Array<String>
-    private lateinit var title : Array<String>
-    private lateinit var detail: Array<String>
+    private lateinit var viewModel: AlbumViewModel
+    private var viewModelAdapter: AlbumsAdapter? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_albums, container, false)
+        _binding = AlbumFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModelAdapter = AlbumsAdapter()
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        dataInitialize()
-        val layoutManager = LinearLayoutManager(context)
-        recyclerView = view.findViewById(R.id.recycler_view_albums_list)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.setHasFixedSize(true)
-        adapter = AlbumAdapter(albumList)
-        recyclerView.adapter = adapter
+        recyclerView = binding.albumsRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
     }
 
-    private fun dataInitialize(){
-        albumList = arrayListOf<Album>()
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        activity.actionBar?.title = getString(R.string.title_albums)
+        viewModel = ViewModelProvider(this, AlbumViewModel.Factory(activity.application)).get(AlbumViewModel::class.java)
+        viewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> {
+            it.apply {
+                viewModelAdapter!!.albums = this
+            }
+        })
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
-        // TODO: This information will be updated with the API call when integrating.
-        title = arrayOf("Buscando América", "Poeta del pueblo", "A Night at the Opera", "A Day at the Races")
-        detail = arrayOf("Salsa", "Salsa", "Rock", "Rock")
-        image = arrayOf("https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg", "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg", "https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png", "https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg")
-
-        for (i in image.indices) {
-            val album = Album(title[i], detail[i], image[i])
-            albumList.add(album)
+    private fun onNetworkError() {
+        if(!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # moviles-202215
+
+Para construir la aplicación de forma local, se debe clonar el repositorio y posteriormente abrirlo el proyecto en Android Studio.
+
+- A partir de la anterior, se debe usar la opción : Build > Make project Ctrl + F9, para construir la aplicación
+- De igual manera, se debe usar la opción : Run > Run 'app' Mayús + F10, para ejecutar la app en el emulador o en el celular de prueba
+
