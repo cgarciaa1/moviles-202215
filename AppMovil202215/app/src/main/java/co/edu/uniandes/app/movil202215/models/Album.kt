fea6f2b22b3ba65fef403dc21ece0658b08c6458
Andres Ramirez Restrepo <a.ramirezr2@uniandes.edu.co>
2022-11-02 21:10:59 -0500

    album-detail: Se agrega modelo del track y se modifica modelo del album para incluir array de tracks.

--- AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/models/Album.kt
+++ AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/models/Album.kt
@@ -1,5 +1,7 @@
 package co.edu.uniandes.app.movil202215.models
 
+import co.edu.uniandes.app.movil202215.view.TrackModel
+
 data class Album (
     val albumId:Int,
     val name:String,
@@ -7,5 +9,6 @@ data class Album (
     val releaseDate:String,
     val description:String,
     val genre:String,
-    val recordLabel:String
+    val recordLabel:String,
+    var tracks: Array<Track>,
 )
---
+++ AppMovil202215/app/src/main/java/co/edu/uniandes/app/movil202215/view/TrackModel.kt
@@ -0,0 +1,3 @@
+package co.edu.uniandes.app.movil202215.view
+
+data class TrackModel()
