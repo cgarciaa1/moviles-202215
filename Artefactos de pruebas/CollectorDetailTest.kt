e5274cdd98fce758a253a43b096a70afc245f0c0
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-12-02 09:39:00 -0500

    * Apk * artefactos de pruebas

---
+++ Artefactos de pruebas/CollectorDetailTest.kt
@@ -0,0 +1,51 @@
+package co.edu.uniandes.app.movil202215
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import co.edu.uniandes.app.movil202215.view.MainActivity
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CollectorDetailTest {
+
+
+    @Rule
+    @JvmField
+    var mActivityTestRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule (MainActivity::class.java)
+
+    @Test
+    fun getCollectorDetailHu06() {
+
+        Thread.sleep(10000)
+        onView(allOf(withResourceName("collectorsFragmentNav"),
+            isDisplayed()))
+            .perform(click())
+
+        Thread.sleep(3000)
+        onView(allOf(withId(R.id.card_view_layout_text_title_collector), withText("Manolo Bellon"),isDisplayed())).perform(click())
+
+        Thread.sleep(3000)
+        val collector = onView(allOf(withId(R.id.detail_layout_title_collector), withText("Manolo Bellon"),isDisplayed()))
+        collector.check(ViewAssertions.matches(isDisplayed()))
+
+        Thread.sleep(3000)
+
+
+    }
+}
